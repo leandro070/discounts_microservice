869031539111a8b5aef0be9a22ae3a9c11186583
Leandro Gutierrez <leandrogutierrez070@gmail.com>
2019-10-09 00:08:37 -0300

    initialize mongodb connection

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+	cMongo := getMongoClient()
+
 	r := gin.Default()
 
 	v1 := r.Group("/v1")
@@ -13,4 +22,26 @@ func main() {
 	}
 
 	r.Run(":3030")
+}
+
+func getMongoClient() *mongo.Client {
+	clientOpt := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.NewClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Connect(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.Background(), readpref.Primary())
+	if err != nil {
+		log.Fatal("Couldn't connect to the Mongo database", err)
+	} else {
+		log.Println("MongoDb connected!")
+	}
+
+	return client
 }
